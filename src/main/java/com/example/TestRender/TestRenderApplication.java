e9591f0dbc8299711b93da7834a64036c09c31b8
fouzaw <fouzi.lezzar@univ-constantine2.dz>
2025-11-13 12:22:19 +0100

    x

---
+++ src/main/java/com/example/TestRender/TestRenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestRender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRenderApplication.class, args);
+	}
+
+}
